a9c11bbd1297b9afb7e4713efb7adea3581783bd
Ross Bell <rossallenbell@gmail.com>
2013-01-20 15:00:57 -0600

    The debugging meta-view render layer is now controlled by a runtime argument built into the debug script

--- src/main/java/com/rossallenbell/strifebasin/StrifeBasin.java
+++ src/main/java/com/rossallenbell/strifebasin/StrifeBasin.java
@@ -5,11 +5,17 @@ import com.rossallenbell.strifebasin.ui.Window;
 
 public class StrifeBasin {
     
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
     
     public static Window window;
     
     public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("debug")) {
+                DEBUG = true;
+            }
+        }
+        
         window = Window.getInstance();
     }
     
